548ef2f7a01c6788f82fb71fd9eead71368aa55d
Tatiana Shavrina <rybolos@gmail.com>
2017-09-28 18:05:32 +0300

    word2vec on vk

---
+++ vw_vk.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import gensim
+import re
+from gensim.models.word2vec import Word2Vec
+
+sentences =  open(r'vktexts.txt', 'r', encoding='utf-8').read()
+sentences = [re.sub("[\W_]+", "", sent) for sent in sentences]
+sentences = [sent.split() for sent in sentences]
+
+model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=4, iter =10)
+model.save('model_w2v_vk')
+model.wv.save_word2vec_format('model_vk.wv', fvocab='vocab_wv', binary=False)
